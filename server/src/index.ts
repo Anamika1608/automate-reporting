012b8cc15359e9129954d21c599238b9beedfc54
Anamika Aggarwal <anamikaagg07@gmail.com>
2025-07-19 22:42:28 +0530

    Integrate typescript, prisma and postgres in backend

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import 'dotenv/config';
+
+import db from "./config/dbConnection";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express()
+const port = process.env.PORT 
+
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+));
+
+db.connect()
+    .then(() => console.log("Database connected successfully"))
+    .catch(err => console.error("Database connection error:", err));
+
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello there!");
+});
